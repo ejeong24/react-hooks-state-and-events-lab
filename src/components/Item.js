7864540ff132a6d5c5b8bff49e391480049d6666
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-05-02 17:19:27 -0400

    procured deliverables

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react"
 
 function Item({ name, category }) {
+
+  const [cartState, setCartState] = useState(false)
+
+  const handleClick = () => {
+    setCartState(!cartState)
+  }
+
   return (
-    <li className="">
+    <li className={cartState ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={cartState ? "remove" : "add"}>
+        {cartState ? "Remove" : "Add to Cart"}
+        </button>
     </li>
   );
 }
